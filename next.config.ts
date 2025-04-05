c430e7521fa563b3a4c0e5ca6e6291de9cc9e330
Augusto-Zeni <augustozeni955@gmail.com>
2025-04-05 10:51:00 -0300

    feat: first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
